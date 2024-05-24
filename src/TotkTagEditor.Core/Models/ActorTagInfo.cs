a273a1e6ac24b5531d2184b420bb95157dfde410
ArchLeaders <archleadership28@gmail.com>
2024-05-24 11:07:16 -0700

    Stub backend classes

---
+++ src/TotkTagEditor.Core/Models/ActorTagInfo.cs
@@ -0,0 +1,24 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace TotkTagEditor.Core.Models;
+
+public partial class ActorTagInfo(string prefix, string name, string suffix) : ObservableObject
+{
+    [ObservableProperty]
+    private string _prefix = prefix;
+
+    [ObservableProperty]
+    private string _name = name;
+
+    [ObservableProperty]
+    private string _suffix = suffix;
+
+    [ObservableProperty]
+    private ObservableCollection<string> _tags = [];
+
+    public ActorTagInfo(string prefix, string name, string suffix, IEnumerable<string> tags) : this(prefix, name, suffix)
+    {
+        _tags = [.. tags];
+    }
+}

33d9eaff958208e12e699a7fde919aa64ac48b88
ArchLeaders <archleadership28@gmail.com>
2024-05-25 23:43:08 -0700

    Stub BitTable implementation

---
+++ src/TotkTagEditor.Core/Models/TagBitTable.cs
@@ -0,0 +1,24 @@
+﻿using Revrs;
+
+namespace TotkTagEditor.Core.Models;
+
+public class TagBitTable : List<TagBitTableEntry>
+{
+    private readonly TagBitTableEntry[] _entries;
+
+    public TagBitTable(ref RevrsReader reader, int entrySize, int pathCount)
+    {
+        _entries = new TagBitTableEntry[pathCount];
+
+        int bitOffset = 0;
+        for (int i = 0; i < pathCount; i++) {
+            _entries[i] = new TagBitTableEntry(ref reader, entrySize, ref bitOffset);
+        }
+    }
+
+    public IEnumerable<string> GetTags(int index, IEnumerable<string> tags)
+    {
+        TagBitTableEntry entry = _entries[index];
+        return tags.Where((_, i) => entry.HasTag(i));
+    }
+}
---
+++ src/TotkTagEditor.Core/Models/TagBitTableEntry.cs
@@ -0,0 +1,16 @@
+﻿using Revrs;
+
+namespace TotkTagEditor.Core.Models;
+
+public class TagBitTableEntry
+{
+    public TagBitTableEntry(ref RevrsReader reader, int entrySize, ref int bitOffset)
+    {
+
+    }
+
+    public bool HasTag(int tagIndex)
+    {
+        throw new NotImplementedException();
+    }
+}

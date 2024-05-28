e8b9cc7dc10b4fadcc1ef4f3cc3d355bb26d0339
ArchLeaders <archleadership28@gmail.com>
2024-05-28 14:26:05 -0700

    Impl TagDatabase view

--- src/TotkTagEditor/ViewModels/TagDatabaseViewModel.cs
+++ src/TotkTagEditor/ViewModels/TagDatabaseViewModel.cs
@@ -1,22 +1,48 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using CommunityToolkit.HighPerformance.Buffers;
+using CommunityToolkit.Mvvm.ComponentModel;
 using FluentAvalonia.UI.Controls;
 using Revrs.Buffers;
+using System.Text;
 using TotkTagEditor.Core;
 using TotkTagEditor.Extensions;
 using TotkTagEditor.Models;
+using TotkTagEditor.Views;
+using VYaml.Emitter;
 
 namespace TotkTagEditor.ViewModels;
 
 public partial class TagDatabaseViewModel : Document
 {
     [ObservableProperty]
-    private TagDatabase _database;
+    private string _entriesYaml;
+
+    [ObservableProperty]
+    private string _tagsYaml;
 
     public TagDatabaseViewModel(string path, Stream input) : base(path.GetRomfsParentFolderName(), path, Symbol.Tag)
     {
         int size = Convert.ToInt32(input.Length);
         using ArraySegmentOwner<byte> data = ArraySegmentOwner<byte>.Allocate(size);
         input.Read(data.Segment);
-        _database = new(data.Segment);
+        TagDatabase database = new(data.Segment);
+
+        using ArrayPoolBufferWriter<byte> writer = new();
+        Utf8YamlEmitter emitter = new(writer);
+
+        database.WriteYamlEntries(ref emitter);
+        int entriesOffset = writer.WrittenSpan.Length;
+        _entriesYaml = Encoding.UTF8.GetString(writer.WrittenSpan);
+
+        database.WriteYamlTags(ref emitter);
+        _tagsYaml = Encoding.UTF8.GetString(writer.WrittenSpan[entriesOffset..]);
+
+        TagDatabaseView view = new() {
+            DataContext = this
+        };
+
+        view.EntriesTextEditor.Text = _entriesYaml;
+        view.TagsTextEditor.Text = _tagsYaml;
+
+        Content = view;
     }
 }
--- src/TotkTagEditor/Views/TagDatabaseView.axaml.cs
+++ src/TotkTagEditor/Views/TagDatabaseView.axaml.cs
@@ -1,10 +1,39 @@
+using Avalonia;
 using Avalonia.Controls;
+using AvaloniaEdit.TextMate;
+using TextMateSharp.Grammars;
+using TotkTagEditor.ViewModels;
 
 namespace TotkTagEditor.Views;
 public partial class TagDatabaseView : UserControl
 {
+    private const string SCOPE_NAME = "source.yaml";
+    private static readonly RegistryOptions _registryOptions = new(Application.Current?.ActualThemeVariant.Key switch {
+        "Light" => ThemeName.LightPlus,
+        _ => ThemeName.DarkPlus,
+    });
+
     public TagDatabaseView()
     {
         InitializeComponent();
+
+        TextMate.Installation tagsTextEditorTextMateInstallation = TagsTextEditor.InstallTextMate(_registryOptions);
+        tagsTextEditorTextMateInstallation.SetGrammar(SCOPE_NAME);
+        TagsTextEditor.TextChanged += (s, e) => {
+            TextChanged((vm) => vm.TagsYaml = TagsTextEditor.Text);
+        };
+
+        TextMate.Installation entriesTextEditorTextMateInstallation = EntriesTextEditor.InstallTextMate(_registryOptions);
+        entriesTextEditorTextMateInstallation.SetGrammar(SCOPE_NAME);
+        EntriesTextEditor.TextChanged += (s, e) => {
+            TextChanged((vm) => vm.EntriesYaml = EntriesTextEditor.Text);
+        };
+    }
+
+    public void TextChanged(Action<TagDatabaseViewModel> update)
+    {
+        if (DataContext is TagDatabaseViewModel vm) {
+            update(vm);
+        }
     }
 }

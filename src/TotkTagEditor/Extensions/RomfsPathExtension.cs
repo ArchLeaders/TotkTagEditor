bda39dff956e039c41f53602cc438636b5a3ab06
ArchLeaders <archleadership28@gmail.com>
2024-05-28 12:55:48 -0700

    Handle negative romfs index

--- src/TotkTagEditor/Extensions/RomfsPathExtension.cs
+++ src/TotkTagEditor/Extensions/RomfsPathExtension.cs
@@ -7,6 +7,10 @@ public static class RomfsPathExtension
         ReadOnlySpan<char> span = path;
 
         int index = span.LastIndexOf("romfs", StringComparison.OrdinalIgnoreCase);
-        return Path.GetFileName(span[..--index]).ToString();
+
+        return index switch {
+            > -1 => Path.GetFileName(span[..--index]).ToString(),
+            _ => Path.GetFileName(Path.GetDirectoryName(span)).ToString()
+        };
     }
 }

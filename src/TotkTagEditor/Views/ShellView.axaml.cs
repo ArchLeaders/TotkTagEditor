55134c20c789c55c166324851202200ea6f89a9a
ArchLeaders <archleadership28@gmail.com>
2024-05-30 11:09:09 -0700

    Add drag/drop support

--- src/TotkTagEditor/Views/ShellView.axaml.cs
+++ src/TotkTagEditor/Views/ShellView.axaml.cs
@@ -1,4 +1,6 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Platform.Storage;
 using FluentAvalonia.UI.Controls;
 using TotkTagEditor.Models;
 using TotkTagEditor.ViewModels;
@@ -10,6 +12,8 @@ public partial class ShellView : UserControl
     public ShellView()
     {
         InitializeComponent();
+
+        DragRegion.AddHandler(DragDrop.DropEvent, DragDropEvent);
     }
 
 #pragma warning disable IDE0051 // Used by the XAML
@@ -29,4 +33,19 @@ public partial class ShellView : UserControl
         }
     }
 #pragma warning restore IDE0051
+
+    public async void DragDropEvent(object? sender, DragEventArgs e)
+    {
+        if (DataContext is not ShellViewModel vm) {
+            return;
+        }
+
+        if (e.Data.GetFiles() is IEnumerable<IStorageItem> files) {
+            foreach (IStorageFile file in files.Where(x => x.Path.IsFile).Cast<IStorageFile>()) {
+                TagDatabaseViewModel tagDatabaseViewModel = new(file.Path.LocalPath, await file.OpenReadAsync());
+                vm.Documents.Add(tagDatabaseViewModel);
+                vm.Current = vm.Documents[^1];
+            }
+        }
+    }
 }

cb331495c6d90fcdd513844c545ca116e81276d4
ArchLeaders <archleadership28@gmail.com>
2024-09-03 00:24:51 -0700

    Update wasm sdk project

--- src/Clients/TotkTagEditor.Browser/Program.cs
+++ src/Clients/TotkTagEditor.Browser/Program.cs
@@ -1,23 +1,16 @@
-﻿using Avalonia;
+﻿using System.Runtime.Versioning;
+using Avalonia;
 using Avalonia.Browser;
-using FluentIcons.Avalonia.Fluent;
-using System.Runtime.Versioning;
 using TotkTagEditor;
 
 [assembly: SupportedOSPlatform("browser")]
 
-internal partial class Program
+internal sealed partial class Program
 {
-    private static async Task Main()
-    {
-        await BuildAvaloniaApp()
-            .StartBrowserAppAsync("out");
-    }
+    private static Task Main(string[] args) => BuildAvaloniaApp()
+        .WithInterFont()
+        .StartBrowserAppAsync("out");
 
     public static AppBuilder BuildAvaloniaApp()
-    {
-        return AppBuilder.Configure<App>()
-                .UseSegoeMetrics()
-                .WithInterFont();
-    }
+        => AppBuilder.Configure<App>();
 }
